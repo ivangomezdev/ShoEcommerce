6921a29aad50f4ad20ae61e08026c88a04e86d50
ivan <ivansangomez6@gmail.com>
2025-01-23 12:57:25 -0500

    v3

--- src/app/api/order/[productId]/route.ts
+++ src/app/api/order/[productId]/route.ts
@@ -3,11 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { productId: string } }
+  { params }: { params: { productId: string } }
 ) {
-  const { productId } = context.params;
-  console.log(request);
-  
+  const { productId } = params;
+
   // Llamar a la función para obtener la orden por ID
   const order = await getOrderById(productId);
 
