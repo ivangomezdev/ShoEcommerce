13c4924397e9c9e6897b6bad4e55a05e6c48ab9e
ivan <ivansangomez6@gmail.com>
2025-01-23 13:00:08 -0500

    v3

--- src/app/api/order/[productId]/route.ts
+++ src/app/api/order/[productId]/route.ts
@@ -1,11 +1,13 @@
 import { getOrderById } from "@/controllers/orderBuy";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { productId: string } }
-) {
-  const { productId } = params;
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const productId = searchParams.get('productId');
+
+  if (!productId) {
+    return NextResponse.json({ error: "Product ID is required" }, { status: 400 });
+  }
 
   // Llamar a la función para obtener la orden por ID
   const order = await getOrderById(productId);
