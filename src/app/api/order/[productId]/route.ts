f550d21dfad285f716cb8374cee19d6a00662cc8
ivan <ivansangomez6@gmail.com>
2025-01-23 12:38:54 -0500

    v3

--- src/app/api/order/[productId]/route.ts
+++ src/app/api/order/[productId]/route.ts
@@ -1,12 +1,13 @@
 import { getOrderById } from "@/controllers/orderBuy";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  
+  request: NextRequest,
   { params }: { params: { productId: string } }
 ) {
   const { productId } = params;
-
+  console.log(request);
+  
   const order = await getOrderById(productId);
 
   return NextResponse.json(order);
